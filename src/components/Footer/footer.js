e7f3a6e4b959094c384828903f33f28b5588d67c
Amil Kahrovic <kahrovicamil11@gmail.com>
2023-07-28 18:58:14 +0200

    povezao stranice

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,20 +1,30 @@
 import React from "react";
-import './footer.css'
+import "./footer.css";
 
+const Footer = () => {
+  return (
+    <div className="containerFooter">
+      <div className="footerBar">
+        <li>
+          <a href="/">Website</a>
+        </li>
+        <li>
+          <a href="/about_us">Users</a>
+        </li>
+        <li>
+          <a href="/coins_page">Coins</a>
+        </li>
+        <li>
+          <a href="/exchanges_page">Exchanges</a>
+        </li>
+      </div>
+      <p>
+        Crypto app made by: Amil Kahrović, Ahmed Vučelj, Ajsa Nicević, Esma
+        Bećović;
+      </p>
+      <p>© 2023 Crypto App</p>
+    </div>
+  );
+};
 
-const Footer = () =>{
-    return(
-        <div className="containerFooter">
-            <div className="footerBar">
-                <li><a href="">Website</a></li>
-                <li><a href="">Users</a></li>
-                <li><a href="">Coins</a></li>
-                <li><a href="">Exchanges</a></li>
-            </div>
-            <p>Crypto app made by: Amil Kahrović, Ahmed Vučelj, Ajsa Nicević, Esma Bećović;</p>
-            <p>© 2023 Crypto App</p>
-        </div>
-    )
-}
-
-export default Footer
+export default Footer;
