496d9b51ec95fcd8596f2c52c81dbef19ae46ab5
Amil Kahrovic <kahrovicamil11@gmail.com>
2023-07-28 16:22:52 +0200

    promene zarad favorites stranice

--- src/components/pages/Exchanges/Exchanges.js
+++ src/components/pages/Exchanges/Exchanges.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useFavoriteCoins } from "../../../contexts/FavoriteCoinsContext";
 import { Table, Image } from "antd";
 import "./Exchanges.css";
 const Exchanges = () => {
